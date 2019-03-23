7ba8469d80fe77d844c1c6484fb82be338f6d636
congpham97 <48613406+congpham97@users.noreply.github.com>
2019-03-24 01:15:52 +0700

    includes ổn rồi đó m, t có sửa lại 1 chút

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Contact: 0398.555.993</b>
+      </div>
+      <strong>Address: 926 Nguyễn Kiệm - Gò Vấp </br> Facebook: <a href="https://www.facebook.com/profile.php?id=100011557027559"> Keita </a></strong>
+    </div>
+</footer>
---
+++ includes/sidebar.php
@@ -0,0 +1,35 @@
+<div class="row">
+	<div class="box box-solid">
+	  	<div class="box-header with-border">
+	    	<h3 class="box-title"><b>Most Viewed Today</b></h3>
+	  	</div>
+	  	<div class="box-body">
+	  		<ul id="trending">
+	  		<?php
+	  			$now = date('Y-m-d');
+	  			$conn = $pdo->open();
+
+	  			$stmt = $conn->prepare("SELECT * FROM products WHERE date_view=:now ORDER BY counter DESC LIMIT 10");
+	  			$stmt->execute(['now'=>$now]);
+	  			foreach($stmt as $row){
+	  				echo "<li><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></li>";
+	  			}
+
+	  			$pdo->close();
+	  		?>
+	    	<ul>
+	  	</div>
+	</div>
+</div>
+
+<!--Nut like fb chua lam dc -->
+<div class="row">
+	<div class='box box-solid'>
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title'><b>Like for me <3</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+	  	</div>
+	</div>
+</div>
